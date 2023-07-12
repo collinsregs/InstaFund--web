1419e8039148c2453f27b10e0473227151dd6ef1
collinsregs <collinsregs@gmail.com>
2023-07-12 15:44:29 +0300

    input modules done

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./database/instafund_database.db');
 
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
 const sql_register_customer = 'INSERT INTO CUSTOMER (National_Id, First_Name, Last_Name, Gender) VALUES (?, ?, ?, ?)'
 var id, first_name, last_name, gender;
 
-function register_customer(callback){
-    rl.question('Enter ID number: ', (id) => {
-        rl.question('Enter first name: ', (first_name) => {
-          rl.question('Enter last name: ', (last_name) => {
-            rl.question('Enter gender: ', (gender) => {
-              rl.close();
-
-    var data=[id,first_name,last_name, gender];
+function register_customer(){
+    
+    var data=[11001100,'collins','red', 'male'];
 db.serialize(()=>{
     db.run(sql_register_customer,data,function(err){
         if (err) {
@@ -29,16 +18,7 @@ db.serialize(()=>{
     });
 
     });
-            })
-          })
-        })
-    })
-}
+  }
 
-register_customer((err)=>{
-    if (err) {
-        console.error('Error registering customer:', err);
-      } else {
-        console.log('Customer registration complete.');
-      }
-});
+register_customer();
+db.close();
---
+++ modules/loan_application.js
@@ -0,0 +1,28 @@
+var db_Connection = require("./db_connect.js");
+console.log(db_Connection);
+
+const sql_loan_application = 'INSERT INTO LOANS (Customer_Id, Credit_Officer_Id, Date, Guarantor_1_Id) VALUES (?, ?, ?, ?)'
+
+
+function apply_loan(Customer_Id,Credit_Officer_Id,Guarantor_1_Id){
+var date_unformatted = new Date();
+var day= date_unformatted.getDate();
+var month= date_unformatted.getMonth();
+var year= date_unformatted.getFullYear();
+var date= `${year}/${month}/${day}` ;
+
+    var data=[Customer_Id,Credit_Officer_Id,date,Guarantor_1_Id];
+db_Connection.serialize(()=>{
+    db_Connection.run(sql_loan_application,data,function(err){
+        if (err) {
+            console.error(err);
+          } else {
+            console.log('Data inserted successfully. Row ID:', this.lastID);
+          }
+    });
+
+    });
+  }
+
+  apply_loan(11001100,1100002, 11001100);
+db_Connection.close();
